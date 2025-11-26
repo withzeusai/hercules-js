d742b94bf490e9c1418c2e5b6e36732f960abc0e
Grant Gurvis <grantgurvis@gmail.com>
2025-11-25 18:47:51 -0800

    fix eslint plugin

--- packages/eslint-plugin-hercules/tsdown.config.ts
+++ packages/eslint-plugin-hercules/tsdown.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: "esm",
   dts: true,
   clean: true,
   minify: false,
+  exports: true,
   sourcemap: true,
   external: [
     "@typescript-eslint/utils",
